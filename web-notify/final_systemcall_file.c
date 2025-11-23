127488f58fbb90ddfe34bb43ce89afce193307ca
dhruv dhiman <dhruvdhiman770@gmail.com>
2025-11-23 09:16:06 +0530

    For all files the core structure is made

---
+++ web-notify/final_systemcall_file.c
@@ -0,0 +1,26 @@
+/*
+ * Aim of file:
+ *  To combine the notification and network file into one for proper working of the systemcall
+ * */
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+//======================================= Space for macro or global variables ====================================//
+
+#define EXT_SUCCESS 0
+#define EXT_FAILURE 1
+
+//======================================= Space for user made function =========================================//
+
+
+//===================================== Space only for main function ============================================//
+
+int main() {
+
+    exit(EXT_SUCCESS);
+}
---
+++ web-notify/notification.c
@@ -0,0 +1,26 @@
+/*
+ * Aim of file:
+ *  To sent the notification to the desktop of the user
+ * */
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+//======================================= Space for macro or global variables ====================================//
+
+#define EXT_SUCCESS 0
+#define EXT_FAILURE 1
+
+//======================================= Space for user made function =========================================//
+
+
+//===================================== Space only for main function ============================================//
+
+int main() {
+
+    exit(EXT_SUCCESS);
+}
