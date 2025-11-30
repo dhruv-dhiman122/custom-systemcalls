76a7ee49c2639d4588de1c4c5ce044eeef604a20
dhruv dhiman <dhruvdhiman770@gmail.com>
2025-11-30 17:00:22 +0530

    The basic structure for code is written

---
+++ FileDict/dict_manipulation.cpp
@@ -0,0 +1,22 @@
+/**/
+
+
+#include <iostream>
+#include <cstdlib>
+#include <dirent.h>
+#include <unistd.h>
+
+//=========================================================== Space for macro and global variables ============================================//
+
+#define EXT_SUCCESS 0
+#define EXT_FAILURE 1
+
+//=========================================================== Space for user defined function ===============================================//
+
+
+//=========================================================== Space for main function =====================================================//
+
+int main(int argc, char* argv[]) {
+
+    exit(EXT_SUCCESS);
+}
